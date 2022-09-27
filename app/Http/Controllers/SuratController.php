03ab832a7d0978649331e4a83859b2c3920c9f4c
ferdyhape <ferdyhahan5@gmail.com>
2022-09-28 03:36:27 +0700

    add the SuratController

--- app/Http/Controllers/SuratController.php
+++ app/Http/Controllers/SuratController.php
@@ -3,82 +3,100 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\category;
+use App\Models\surat;
+use Carbon\Carbon;
 
 class SuratController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $surat = surat::all();
+
+        // mengirim data ke view index
+        return view('dashboard.index', [
+            'surat' => surat::all(),
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('dashboard.create', [
+            'category' => category::all()
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+
+        $validatedData = $request->validate([
+            'letter_number' => 'required|min:3|max:255',
+            'title' => 'required|min:3|max:255',
+            'category_id' => 'required|integer',
+        ]);
+        $validatedData['archive_time'] = Carbon::now();
+
+        // if ($request->file('gambar')) {
+        //     $nama_gambar = $request->file('gambar')->store('product-images');
+        // }
+
+        // $validatedData['gambar'] = $nama_gambar;
+
+        // dd($validatedData);
+        surat::create($validatedData);
+
+        // $request->session()->flash('success', 'Data surat berhasil ditambahkan');
+
+        return redirect('/surat')->with('success', 'Data surat berhasil ditambahkan');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(surat $surat)
     {
-        //
+        // return view('dashboard.surat.detail-surat');
+        return view('dashboard.show', [
+            'surat' => $surat
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit(surat $surat)
     {
-        //
+        return view('dashboard.update', [
+            'surat' => $surat,
+            'category' => Category::all(),
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(Request $request, surat $surat)
     {
-        //
+        $rules = ([
+            'letter_number' => 'required|integer',
+            'title' => 'required|min:3|max:255',
+            'category_id' => 'required|integer',
+        ]);
+
+        $validatedData = $request->validate($rules);
+        $validatedData['archive_time'] = Carbon::now();
+
+        // if ($request->file('gambar')) {
+        //     if ($request->gambarLama) {
+        //         Storage::delete($request->gambarLama);
+        //     }
+        //     $nama_gambar = $request->file('gambar')->store('product-images');
+        // }
+        // $validatedData['gambar'] = $nama_gambar;
+
+        surat::where('id', $surat->id)->update($validatedData);
+
+        $request->session()->flash('success', 'Data surat berhasil diupdate');
+
+        return redirect('/surat');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+
+    public function destroy(surat $surat)
     {
-        //
+        surat::destroy($surat->id);
+        return redirect('/surat')->with('success', 'Data surat berhasil dihapus');
     }
 }
